5f4302f53988f0c0bbe26a5ffed7047a30f6bc4e
Işıl Özkaynak <59703446+isilozkaynak@users.noreply.github.com>
2021-09-04 15:49:41 +0300

    added routing

--- gamewebsite/src/app/components/category/category.component.ts
+++ gamewebsite/src/app/components/category/category.component.ts
@@ -9,10 +9,35 @@ import { CategoryService } from 'src/app/services/category.service';
 })
 export class CategoryComponent implements OnInit {
 
-  category : Category[]=[];
+  categories : Category[] = [];
+  currentCategory : Category;
+  dataLoaded = false;
+
   constructor(private categoryService : CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories(){
+    this.categoryService.getCategories().subscribe(response=>{
+      this.categories = response.data
+      this.dataLoaded=true;
+    })
+  }
+
+  setCurrentCategory(category:Category){
+    //console.log(category.categoryName);
+    this.currentCategory = category;
+  }
+
+  getCurrentCategoryClass(category:Category){
+    if(category==this.currentCategory){
+      return "list-group-item-active";
+    }
+    else {
+      return "list-group-item";
+    }
   }
 
 }
